d2000747793017a15b11c30185b5ce479c552638
suman38 <103047484+suman38@users.noreply.github.com>
2024-08-15 21:28:35 +0530

    taking inputs in swing first commit

--- RPG1/src/com/suman/game/Game.java
+++ RPG1/src/com/suman/game/Game.java
@@ -2,11 +2,16 @@ package com.suman.game;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 import com.suman.game.art.Art;
 
@@ -19,13 +24,28 @@ public class Game extends JPanel {
 
 	private int playerMoveSpeed = 5;
 
+	private final int IFW = JComponent.WHEN_IN_FOCUSED_WINDOW;
+	private final String MOVE_UP = "move player up";
+	private final String MOVE_DOWN = "move player down";
+
 	private Art art;
-	
+
 	// This is for the rendering the game
 	public Game() {
-		
+
+		// Arrow keys
+		this.getInputMap(IFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0, false), MOVE_UP);
+		this.getInputMap(IFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0, false), MOVE_DOWN);
+
+		// W and S
+		this.getInputMap(IFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, false), MOVE_UP);
+		this.getInputMap(IFW).put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, false), MOVE_DOWN);
+
+		this.getActionMap().put(MOVE_UP, new MovePlayerAction(1,this));
+		this.getActionMap().put(MOVE_DOWN, new MovePlayerAction(2,this));
+
 		art = new Art();
-		
+
 		try {
 			img1 = ImageIO.read(getClass().getResource("/playerimages/player.png"));
 		} catch (IOException e) {
@@ -39,8 +59,7 @@ public class Game extends JPanel {
 	}
 
 	public void tick() {
-		x += playerMoveSpeed;
-		// y += playerMoveSpeed;
+
 	}
 
 	@Override
@@ -49,11 +68,46 @@ public class Game extends JPanel {
 
 		Graphics2D g2 = (Graphics2D) g;
 		g2.drawImage(img1, x, y, width, height, null);
-		
-		g2.drawImage(art.playerDown, x, 0, art.artResize, art.artResize, null);
-		g2.drawImage(art.grass,100,0,art.artResize,art.artResize,null);
-		
-		
+
+		g2.drawImage(art.playerDown, x+100, y, art.artResize, art.artResize, null);
+		g2.drawImage(art.grass, 100, 0, art.artResize, art.artResize, null);
+
 		g2.dispose();
+	}
+
+	public void movePlayerUp() {
+		y -= playerMoveSpeed;
+	}
+	
+	public void movePlayerDown()
+	{
+		y += playerMoveSpeed;
+	}
+}
+
+class MovePlayerAction extends AbstractAction {
+
+	private static final long serialVersionUID = 1L;
+
+	private Game game;
+	private int direction;
+
+	public MovePlayerAction(int direction, Game game) {
+		this.direction = direction;
+		this.game = game;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (direction) {
+		case 1:
+			//up
+			game.movePlayerUp();
+			break;
+		case 2:
+			//down
+			game.movePlayerDown();
+			break;
+		}
 	}
 }
